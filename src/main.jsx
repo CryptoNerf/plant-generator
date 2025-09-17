0b55b7f247d3d85cf12388ca473bcc96d64be1b1
CryptoNerf <anuta1711_98@mail.ru>
2025-09-17 20:41:38 +0400

    add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from "./App.jsx";
 import './index.css'
+import { Analytics } from '@vercel/analytics/react' // Импорт компонента аналитики
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Analytics /> {/* Добавляем компонент аналитики */}
     <App />
   </React.StrictMode>,
 )
